0241c54d9c09fb3c6b3f2f0998470323dac6198d
TheAlphaApp <souravsuman636@gmail.com>
2022-03-26 21:53:55 +0530

    temp page skeleton added

---
+++ lib/features/temp_converter/presentation/temp_conv_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TempConvPage extends StatefulWidget {
+  const TempConvPage({Key? key}) : super(key: key);
+
+  @override
+  State<TempConvPage> createState() => _TempConvPageState();
+}
+
+class _TempConvPageState extends State<TempConvPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
